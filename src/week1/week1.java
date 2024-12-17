2e087b8ed33856f42b9ac0b2479b1cfc6901934c
azucena00 <Azucenae16@yahoo.com>
2024-12-16 21:46:42 -0700

    first commit being made

---
+++ src/week1/week1.java
@@ -0,0 +1,23 @@
+package week1;
+
+public class week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// this is a comment 
+		int numberOfFriends; 
+	int samsAge  =  26; 
+	System.out.println(samsAge);
+	double accountBalance = 34.67;
+	System.out.println( accountBalance);
+	char middleInitial = 'C';
+	boolean isHotOutside = true;
+	String firstName = "Sam";
+	
+	System.out.println(middleInitial);
+	System.out.println(isHotOutside);
+	System.out.println( "Sam");
+	}
+	
+
+}
